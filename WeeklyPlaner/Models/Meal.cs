330be43c66db4c3a5c6605a6dd588439af1c84cc
farzinmaleknia <farzin.en@gmail.com>
2024-06-02 17:52:28 +0330

    working on UI

--- WeeklyPlaner/Models/Meal.cs
+++ WeeklyPlaner/Models/Meal.cs
@@ -4,7 +4,7 @@
     {
         public string Title { get; set; }
         public string FoodName { get; set; }
-
+        public bool GroceryEditable { get; set; } = false;
         public List<Grocery> Groceries { get; set; }
     }
 }
