c883cde157abd7301f7ff0a1e499bb386ae63e5b
farzinmaleknia <farzin.en@gmail.com>
2024-06-05 16:15:31 +0330

    Working on Colors and Fonts

--- WeeklyPlaner/Models/Week.cs
+++ WeeklyPlaner/Models/Week.cs
@@ -5,12 +5,12 @@ namespace WeeklyPlaner.Models
     public class Week
     {
         public int Id { get; set; } = 0;
-        public Day Thursday { get; set; } = new Day(){ Title = "پنج شنبه",};
+        public Day Thursday { get; set; } = new Day(){ Title = "پنج‌شنبه",};
 		public Day Friday { get; set; } = new Day() { Title = "جمعه",};
 		public Day Saturday { get; set; } = new Day() { Title = "شنبه" };
         public Day Sunday { get; set; } = new Day() { Title = "یکشنبه" };
         public Day Monday { get; set; } = new Day() { Title = "دوشنبه" };
-        public Day Tuesday { get; set; } = new Day() { Title = "سه شنبه" };
+        public Day Tuesday { get; set; } = new Day() { Title = "سه‌شنبه" };
         public Day Wednesday { get; set; } = new Day() { Title = "چهارشنبه" };
 
         public List<string> Colors { get; } = new List<string>() { "blue", "red", "green", "orange", "purple", "mustard", "skyblue" };
